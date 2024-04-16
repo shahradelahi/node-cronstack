bbced0992ad55a7ee9ae11213e1e5bf55fcdc48a
Shahrad Elahi <shahradq@gmail.com>
2024-04-16 23:39:10 +0330

    fix lint

--- packages/cronstack/tests/utils.test.ts
+++ packages/cronstack/tests/utils.test.ts
@@ -1,14 +1,16 @@
 import { fsAccess } from '@/utils/fs-access';
 import { readDirectory } from '@/utils/read-directory-files';
+import { expect } from 'chai';
 
 describe('utils', () => {
   it('should read directory files', async () => {
     const res = await readDirectory('.');
-    console.log(res);
+    expect(res.data).to.be.an('array');
+    expect(res.data!.find((x) => x.basename === 'node_modules')).to.not.be.undefined;
   });
 
   it('should check if a file exists', async () => {
     const res = await fsAccess('.');
-    console.log(res);
+    expect(res).to.be.true;
   });
 });
